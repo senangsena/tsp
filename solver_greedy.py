8f55d2338544fde618eb9f567d5ec6cd72a91889
Sena LEE <sena@SenanoMacBook-Air.local>
2026-06-18 12:06:59 +0900

    関数のモジュール化

--- solver_greedy.py
+++ solver_greedy.py
@@ -7,50 +7,60 @@ import math
 from common import print_tour, read_input
 
 
+class Solve_greedy:
 
-# cities: 座標のリスト
-# 座標のindexで表した、訪れる順番リスト（[0, 4, 2, ...]なら、0番目の座標からスタート、４番目→２番目...と進むということ
-def solve_greedy(cities):
-
+    def __init__(self, cities):
+        self.cities = cities
      
     # start, goalは(x, y)の座標
-    def calc_distance(start, goal):
+    def calc_distance(self, start, goal):
         return math.sqrt((start[0] - goal[0])**2 + (start[1] - goal[1])**2)
-
-    # 訪れる順番を保存するorder
-    order = [0]
-
-    # cities[cirrent_index]を今見ているということ
-    current_index = 0
-
-    unvisitted_ids = [i for i in range(len(cities))]
-
-    while len(unvisitted_ids) > 1:
-
+    
+    # inthisAreaの中で、city_idに最も近いcityのidを返す
+    def find_nearest_city(self, city_id: int, unvisitted_ids: list[int]) -> int:
         # 最短距離を入れるmin_distance
         min_distance = float('inf')
 
-        # 未訪問のcityリスト。自分自身にいくことはできないので取り除く
-        unvisitted_ids.remove(current_index)
-        
-
-        for candidate_id in unvisitted_ids: # city は座標
+        for candidate_id in unvisitted_ids: 
 
             # 2点間の距離を計算
-            d = calc_distance(cities[current_index], cities[candidate_id])
+            d = self.calc_distance(self.cities[city_id], self.cities[candidate_id])
 
             # 最短距離の更新
             if min_distance > d:
                 min_distance = d
                 next_index = candidate_id # ここに最後に入っているindexが次に進むcityのindex
-        
-        order.append(next_index)
-        current_index = next_index
 
-    return order
+        # 移動先のcityのidを返す
+        return next_index
+        
+    
+    # greedyな解き方で訪れる順番を求める。このとき、各移動の経路長も保存し、順番とともに返す
+    def greedy(self):
+
+        # city間の移動の順番のリスト 3 -> 5 -> 1 なら、[3, 5, 1]
+        self.order = [0]
+
+        # cities[cirrent_index]を今見ているということ
+        current_index = 0
+
+        unvisitted_ids = [i for i in range(len(self.cities))]
+
+        while len(unvisitted_ids) > 1:
+
+            # 未訪問のcityリスト。自分自身にいくことはできないので取り除く
+            unvisitted_ids.remove(current_index)
+            
+            next_index = self.find_nearest_city(current_index, unvisitted_ids)
+            
+            self.order.append(next_index)
+            current_index = next_index
+
 
 
 if __name__ == '__main__':
     assert len(sys.argv) > 1
-    tour = solve_greedy(read_input(sys.argv[1]))
+    s = Solve_greedy(read_input(sys.argv[1]))
+    s.greedy()
+    tour = s.order
     print_tour(tour)
